c77f6a6420ca858be25192135e1af29a9afd9315
NageshKanneganti <nk726@njit.edu>
2024-02-02 22:57:53 -0600

    updated calculator/__init__.py

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,9 @@
+"""My Caculator Functions"""
+
 def add(a,b):
+    """This function does addition"""
     return a + b
 
 def subtract(a,b):
+    """This function does subtraction"""
     return a - b
