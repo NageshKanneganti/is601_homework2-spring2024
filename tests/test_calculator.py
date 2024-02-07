414e9f4f8a99bb0d37629781c5cdec80c9c409c9
NageshKanneganti <nk726@njit.edu>
2024-02-06 19:17:59 -0600

    Updated test calculator functions

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import add, subtract, multiply, divide, remainder
 
 def test_addition():
     '''Test that addition function works '''    
@@ -8,3 +8,15 @@ def test_addition():
 def test_subtraction():
     '''Test that addition function works '''    
     assert subtract(2,2) == 0
+
+def test_multiplication():
+    '''Test that multiplication function works '''    
+    assert multiply(2,2) == 4
+
+def test_division():
+    '''Test that division function works '''    
+    assert divide(2,2) == 1
+
+def test_remainder():
+    '''Test that modulus function works '''    
+    assert remainder(2,2) == 0
