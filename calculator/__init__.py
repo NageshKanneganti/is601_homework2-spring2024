74b7e163fc1a16e49c58800bb07de3331fed1254
NageshKanneganti <nk726@njit.edu>
2024-02-06 19:14:45 -0600

    Updated calculator functions

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -7,3 +7,15 @@ def add(a,b):
 def subtract(a,b):
     """This function does subtraction"""
     return a - b
+
+def multiply(a,b):
+    """This function does multiplication"""
+    return a * b
+
+def divide(a,b):
+    """This function does division"""
+    return a / b
+
+def remainder(dividend,divisor):
+    """This function does modulus"""
+    return dividend % divisor
